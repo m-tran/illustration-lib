6947cac629cb63c0296859d0b688d3454e0ebeeb
m-tran <mptran0101@berkeley.edu>
2021-05-30 13:39:16 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const cors = require('cors');
+const path = require('path');
+const config = require("./config/config");
