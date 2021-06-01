b254eece5bb67e0f73c8c76499b70713985d4b3e
Michelle Tran <mptran0101@berkeley.edu>
2021-05-31 20:17:00 -0700

    added ESLint

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 
 // Opt-in to Webpack hot module replacement
-if (module.hot) module.hot.accept()
+if (module.hot) module.hot.accept();
 
+/* eslint-disable no-undef */
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -65,6 +65,27 @@ module.exports = {
                     'postcss-loader'
                 ]
             },
+            {
+                test: /\.(jsx|js)$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                "targets": "defaults"
+                            }],
+                            '@babel/preset-react'
+                        ]
+                    }
+                }, {
+                    loader: 'eslint-loader',
+                    options: {
+                        fix: true
+                    }
+                }]
+            },
         ],
     },
 }
