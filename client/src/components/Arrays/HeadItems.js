0ec79b9aaa54ec3405b0ffeb54521ae41b5ae93a
Michelle Tran <mptran0101@berkeley.edu>
2021-06-29 23:30:00 -0700

    inserted temp image array for carousel

--- client/src/components/Arrays/HeadItems.js
+++ client/src/components/Arrays/HeadItems.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
-// const handleDragStart = (e) => e.preventDefault();
+import hijab from '../../images/heads/Hijab.png';
+import shortWavy from '../../images/heads/Short-Wavy.png';
+import sidePartTwo from '../../images/heads/Side-Part-02.png';
 
-// const headItems = [
-// 	<img src="path-to-img" alt="" onDragStart={handleDragStart} />,
-// 	<img src="path-to-img" alt="" onDragStart={handleDragStart} />,
-// 	<img src="path-to-img" alt="" onDragStart={handleDragStart} />,
-// ];
+const handleDragStart = (e) => e.preventDefault();
 
 const headItems = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
+	<img src={hijab} alt="" onDragStart={handleDragStart} />,
+	<img src={shortWavy} alt="" onDragStart={handleDragStart} />,
+	<img src={sidePartTwo} alt="" onDragStart={handleDragStart} />,
+	<div className="item" data-value="4">4</div>,
+	<div className="item" data-value="5">5</div>,
 ];
+
+// const headItems = [
+//     <div className="item" data-value="1">1</div>,
+//     <div className="item" data-value="2">2</div>,
+//     <div className="item" data-value="3">3</div>,
+//     <div className="item" data-value="4">4</div>,
+//     <div className="item" data-value="5">5</div>,
+// ];
 
 export default headItems;
