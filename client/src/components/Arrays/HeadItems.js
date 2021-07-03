f60d6e79c12cbde2a8c0d1e3f4c495999acc77e3
Michelle Tran <mptran0101@berkeley.edu>
2021-07-03 00:36:02 -0700

    added head illustrations

--- client/src/components/Arrays/HeadItems.js
+++ client/src/components/Arrays/HeadItems.js
@@ -1,15 +1,19 @@
 import 'react-alice-carousel/lib/alice-carousel.css';
 
-// import hijab from '../../images/heads/Hijab.png';
-// import shortWavy from '../../images/heads/Short-Wavy.png';
-// import sidePartTwo from '../../images/heads/Side-Part-02.png';
-
 import bobWavy from '../../images/heads/wavy-bob.png';
 import sidePart from '../../images/heads/side-part-01.png';
+import afro from '../../images/heads/afro.png';
+import bald from '../../images/heads/bald.png';
+import turbanOne from '../../images/heads/turban_01.png';
+import turbanTwo from '../../images/heads/turban_02.png';
 
 const headItems = [
 	bobWavy,
 	sidePart,
+	afro,
+	bald,
+	turbanOne,
+	turbanTwo,
 ];
 
 export default headItems;
